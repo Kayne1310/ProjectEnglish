a7e70051e6f66d66b101e67f3ce82e241492d622
PC <khanhdjkl@gmail.com>
2025-01-14 17:49:05 +0700

    create project

--- BE/ProjectEnglishFall2025.Application/obj/Debug/net8.0/ProjectFall2025.Application.AssemblyInfo.cs
+++ BE/ProjectEnglishFall2025.Application/obj/Debug/net8.0/ProjectFall2025.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectFall2025.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb12e0931c43900af1df66ee2f58c131baf29587")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectFall2025.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectFall2025.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BE/ProjectFall2025.Infrastructure/obj/Debug/net8.0/ProjectFall2025.Infrastructure.AssemblyInfo.cs
+++ BE/ProjectFall2025.Infrastructure/obj/Debug/net8.0/ProjectFall2025.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectFall2025.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+945ca89822148659685f34896eccedb3b86fe048")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb12e0931c43900af1df66ee2f58c131baf29587")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectFall2025.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectFall2025.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
